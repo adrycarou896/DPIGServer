899d5d4b664d75de702a52f521c1ef094af0ab95
adrycarou896 <adripc@live.com>
2019-04-12 17:49:00 +0200

    Primer commit

---
+++ src/main/java/com/tfg/dpigserver/DPIGServerApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.dpigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DPIGServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DPIGServerApplication.class, args);
+	}
+
+}
