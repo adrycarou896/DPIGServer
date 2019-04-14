3b1dfa632dec8661179b1bc8fb6e64adcbff0507
adrycarou896 <adripc@live.com>
2019-04-14 23:57:45 +0200

    Mejoras

--- src/main/java/com/uniovi/DPIGServerApplication.java
+++ src/main/java/com/uniovi/DPIGServerApplication.java
@@ -1,13 +1,14 @@
-package com.tfg.dpigserver;
+package com.uniovi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class DPIGServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DPIGServerApplication.class, args);
 	}
-
 }
